2d3125a9e8a5df030ce9bce8b1bd597e7be67d17
unknown <glmaciosek@gmail.com>
2022-08-31 23:15:15 -0300

    global context, pokedex e coordinator

--- guilherme-dir/pokedex/src/pages/Home.js
+++ guilherme-dir/pokedex/src/pages/Home.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../constants/url";
 
 function Home() {
+  const [pokemons, setPokemons] = useState([]);
+
+  axios
+    .get(BASE_URL)
+    .then((response) => {
+      setPokemons(response.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <div>
-    <h1> Home Page PokedeX </h1>
+      <h1> Home Page PokedeX </h1>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.id}>
+          <span>{pokemon.name}</span>
+        </div>
+      ))}
     </div>
-    )
+  );
 }
 
-export default Home
+export default Home;
---
+++ guilherme-dir/pokedex/src/router/coordinator.js
@@ -0,0 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
+export const goToHomePage = useNavigate("/home")
+
+export const goToPokedex = useNavigate("/pokedex")
