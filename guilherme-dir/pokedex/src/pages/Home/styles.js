ac15cdade306a3ae46f71456bd6884511115eb06
rafix923 <rafaellopes.dev@gmail.com>
2022-09-01 19:11:13 -0300

    Atualizar componentes em pastas e fazer o header da Home.js

---
+++ guilherme-dir/pokedex/src/pages/Home/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HomeHeader = styled.header`
+border: solid grey 1px;
+display: flex;
+
+button{
+    height: 5vh;
+    margin-top:20px;
+    margin-left:20px;
+}
+
+h1{
+    margin: 20px auto;
+}
+`
--- guilherme-dir/pokedex/src/router/Router.js
+++ guilherme-dir/pokedex/src/router/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Pokedex from "../pages/Pokedex";
+import Home from "../pages/Home/Home";
+import Pokedex from "../pages/Pokedex/Pokedex";
 
 export function Router() {
   return (
