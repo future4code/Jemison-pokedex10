b5c88b4ac1245b2a3b56ae6a58da1c88fefa6eb3
rafix923 <rafaellopes.dev@gmail.com>
2022-09-09 22:23:08 -0300

    Navegarção entre detailsPage e ultima pagina

--- guilherme-dir/pokedex/src/pages/DetailsPage/DetailsPage.js
+++ guilherme-dir/pokedex/src/pages/DetailsPage/DetailsPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function DetailsPage() {
+
+ const navigate = useNavigate();
+
+ const goToLastPage=()=>{
+  navigate(-1)
+ }
 
   return (
     <div>
         <h1>Details Page</h1>
+        <button onClick={goToLastPage}>Voltar</button>
+        <button>Adicionar/Remover da pokedex</button>
     </div>
   )
 }
