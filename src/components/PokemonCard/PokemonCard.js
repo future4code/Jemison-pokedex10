aeb5627e43cb37f887a9f77d031c2fb286a219f8
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 17:59:59 -0300

    Se adicionar esse commit, adicionará no PokemonCard função para adicionar pokemons à Pokedex e remover da lista de pokemons o pokemon capiturado.

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ButtonsContainer, ImgContainer, PokeCardContainer, PokeImg } from '../PokemonCard/styled';
 import { goToPokemonDetail } from '../../routes/coordinator';
+import { GlobalStateContext } from '../../global/GlobalStateContext';
 
 
 function PokemonCard({ pokemon, isPokedex }) {
 
     const navigate = useNavigate();
 
+    // Para pegar dados do estado global
+    const { pokemons, setPokemons, pokedex, setPokedex } = useContext(GlobalStateContext)
+
+    const addToPokedex = () => {
+        const pokeIndex = pokemons.findIndex((item) => item.name === pokemon.name)
+        const newPokemonsList = [...pokemons]
+        newPokemonsList.splice(pokeIndex, 1)
+        const newPokedexList = [...pokedex, pokemon]
+        setPokedex(newPokedexList);
+        setPokemons(newPokemonsList)
+    };
+
+    const removeFromPokedex = () => {
+
+    }
 
     return (
         <div>
@@ -19,7 +35,7 @@ function PokemonCard({ pokemon, isPokedex }) {
                     />
                 </ImgContainer>
                 <ButtonsContainer>
-                    <button>
+                    <button onClick={isPokedex ? removeFromPokedex : addToPokedex}>
                         {isPokedex ? "Remover da Pokedex" : "Adicionar à Pokedex"}
                     </button>
                     <button onClick={() => goToPokemonDetail(navigate, pokemon.name)}>Ver detalhes</button>
