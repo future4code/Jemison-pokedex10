b6317224e264b4161b924ef2662b59ed387eec11
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 15:58:35 -0300

    Se adicionar esse commit, aplicará condicional para mostrar o botão da direita no Header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HeaderContainer, LeftHeaderButton, RightHeaderButton } from './styled';
+import { goToPokedex } from '../../routes/coordinator'
 
-function Header() {
+// Recebendo as informações por props
+function Header({ title, leftButtonFunction, showRightBotton }) {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Header</div>
+    <HeaderContainer>
+      <LeftHeaderButton>Botão</LeftHeaderButton>
+      <h1>{title}</h1>
+      {/* Para só mostrar o botão da direita se a condição for verdadeira */}
+      {
+        showRightBotton &&
+        <RightHeaderButton onClick={() => goToPokedex(navigate)}>Ir para Pokedex</RightHeaderButton>
+
+      }
+    </HeaderContainer>
   )
 }
 
