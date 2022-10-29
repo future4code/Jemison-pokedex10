722ffdcd804416a8885485b79ea7c760d4728750
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 12:46:49 -0300

    Se adicionar esse commit, irá adicionar estilo inicial ao arquivo PokemonListScreen.

--- src/screens/PokemonListScreen/PokemonListScreen.js
+++ src/screens/PokemonListScreen/PokemonListScreen.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import PokemonCard from '../../components/PokemonCard/PokemonCard';
-
+import { PokeListContainer } from '../PokemonListScreen/styled';
 function PokemonListScreen() {
   return (
     <div>
       <Header />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
+      <PokeListContainer>
+        <PokemonCard />
+      </PokeListContainer>
     </div>
   )
 }
