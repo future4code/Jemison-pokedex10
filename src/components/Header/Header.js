f91c8d51821d14ee2d42db84fc9acea1f9106242
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 16:43:58 -0300

    Se adicionar esse commit, aplicará condicional para definir a navegação do botão da esquerda do Header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { HeaderContainer, LeftHeaderButton, RightHeaderButton } from './styled';
@@ -8,15 +9,28 @@ function Header({ title, leftButtonFunction, showRightBotton }) {
 
   const navigate = useNavigate()
 
+  const leftButtonTitle = () => {
+    switch (title) {
+      case "Lista de Pokemons":
+        return "Ir para Pokedex";
+        break;
+      case "Pokedex":
+        return "Voltar para a lista de pokemons";
+        break;
+      default:
+        return "Voltar";
+        break;
+    }
+  };
+
   return (
-    <HeaderContainer>
-      <LeftHeaderButton>Botão</LeftHeaderButton>
+    <HeaderContainer >
+      <LeftHeaderButton onClick={leftButtonFunction}>{leftButtonTitle()}</LeftHeaderButton>
       <h1>{title}</h1>
       {/* Para só mostrar o botão da direita se a condição for verdadeira */}
       {
         showRightBotton &&
         <RightHeaderButton onClick={() => goToPokedex(navigate)}>Ir para Pokedex</RightHeaderButton>
-
       }
     </HeaderContainer>
   )
