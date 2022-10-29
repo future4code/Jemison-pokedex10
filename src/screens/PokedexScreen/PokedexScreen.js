3d9a690c6f7908b7c38569d5d4f8b106736a1b12
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 17:24:27 -0300

    Se adicionar esse commit, mapeará os pokemons adicionados à Pokedex.

--- src/screens/PokedexScreen/PokedexScreen.js
+++ src/screens/PokedexScreen/PokedexScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { goToPokemonList } from '../../routes/coordinator';
 import { PokeListContainer } from './styled';
+import { GlobalStateContext } from '../../global/GlobalStateContext'
+import PokemonCard from '../../components/PokemonCard/PokemonCard';
 
 function PokedexScreen() {
 
   const navigate = useNavigate()
+
+  const { pokedex } = useContext(GlobalStateContext)
   return (
     <>
       <Header
@@ -14,7 +18,11 @@ function PokedexScreen() {
         leftButtonFunction={() => goToPokemonList(navigate)}
       />
       <PokeListContainer>
-        {}
+        {
+          pokedex && pokedex.map((item) => {
+            return <PokemonCard key={item.name} pokemon={item} />
+          })
+        }
       </PokeListContainer>
     </>
   )
