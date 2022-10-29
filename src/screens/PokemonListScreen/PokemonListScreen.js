17cff302cba2c143f24d8c50c0c1bf09aaff6815
rafix923 <rafaellopes.dev@gmail.com>
2022-10-29 12:29:12 -0300

    Se adicionar esse commit, importará os componentes Header e PokemonCard para o arquivo PokemonListScreen. Isso fará a renderização dos componentes que se repetem nas páginas.

--- src/screens/PokemonListScreen/PokemonListScreen.js
+++ src/screens/PokemonListScreen/PokemonListScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header/Header';
+import PokemonCard from '../../components/PokemonCard/PokemonCard';
 
 function PokemonListScreen() {
   return (
-    <div>PokemonListScreen</div>
+    <div>
+      <Header />
+      <PokemonCard />
+    </div>
   )
 }
 
